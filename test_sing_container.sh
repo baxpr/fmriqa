0bc4ec56bb3e855e8844779f91e1e18aac77bfaf
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2019-05-10 12:40:33 -0500

    First compiled ver

--- test_sing_container.sh
+++ test_sing_container.sh
@@ -10,6 +10,6 @@ t1_file INPUTS/t1.nii.gz \
 seg_file INPUTS/seg.nii.gz \
 fmri_file INPUTS/fmri.nii.gz \
 project UNK_PROJ \
-subject UNK_SUBJ \
-session UNK_SESS \
-scan UNK_SCAN
+subject 600000000001 \
+session 600000000001 \
+scan 50362
