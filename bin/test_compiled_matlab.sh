d2558fd25a19e13a4d6073fefc870263bd737de3
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2019-05-10 12:59:43 -0500

    SPM command line

--- bin/test_compiled_matlab.sh
+++ bin/test_compiled_matlab.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-sh run_spm12.sh /usr/local/MATLAB/MATLAB_Runtime/v92 function fmriqa \
-out_path ../OUTPUTS \
-t1_file ../INPUTS/t1.nii.gz \
-seg_file ../INPUTS/seg.nii.gz \
-fmri_file ../INPUTS/fmri.nii.gz \
-project UNK_PROJ \
-subject 600000000001 \
-session 600000000001 \
-scan 50362
+bash run_spm12.sh /usr/local/MATLAB/MATLAB_Runtime/v92 function fmriqa \
+../OUTPUTS \
+../INPUTS/t1.nii.gz \
+../INPUTS/seg.nii.gz \
+../INPUTS/fmri.nii.gz \
+UNK_PROJ \
+600000000001 \
+600000000001 \
+50362
